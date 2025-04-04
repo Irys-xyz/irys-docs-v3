f3b789e31274302c45d89aac75f1499d670556d1
TheHoltz <holtzs.william@gmail.com>
2025-04-04 12:56:22 -0300

    feat: enhance IrysAddChainButton with error handling and improved UI

--- components/add-chain.tsx
+++ components/add-chain.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState } from "react";
+
 const testnetV1 = {
     chainId: "0x4F6",
     chainName: "Irys Testnet v1",
@@ -14,20 +16,31 @@ const testnetV1 = {
 
 const IrysAddChainButton: React.FC = () => {
 
+    const [error, setError] = useState<string | null>(null);
+
     const onClick = async () => {
         if (window.ethereum) {
-            await window.ethereum.request({
-                method: 'wallet_addEthereumChain',
-                params: [testnetV1],
-            });
+            try {
+                await window.ethereum.request({
+                    method: 'wallet_addEthereumChain',
+                    params: [testnetV1],
+                });
+            } catch (error) {
+                console.error('Failed to add Irys network:', error);
+                setError('Failed to add Irys Network, please check your wallet and try again.');
+            }
         }
     };
     return (
-        <button
-            onClick={onClick}
-            className="prose prose-invert mx-auto py-8 max-w-7xl">
-            Add Irys
-        </button>
+        <>
+
+            <button
+                onClick={onClick}
+                className="bg-[#51FFD6] text-[#18191A] px-4 py-2 rounded-md">
+                Add Irys Network to Your Wallet
+            </button>
+            {error && <div className="text-red-500 mt-2">{error}</div>}
+        </>
     );
 
 };
